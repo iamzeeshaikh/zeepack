7ac9fcc18e0c88fd88c6e7e2a96b1855c72b3a40
zeeshan shaikh <shanimazhar82@gmail.com>
2026-05-05 12:38:12 +0500

    Add Zendesk chat script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
+import Script from "next/script";
 
 import { AnnouncementBar } from "@/components/layout/announcement-bar";
 import { Footer } from "@/components/layout/footer";
@@ -54,6 +55,18 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <Script id="zendesk-chat" strategy="afterInteractive">
+          {`
+            window.$zopim||(function(d,s){var z=$zopim=function(c){
+            z._.push(c)},$=z.s=
+            d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+            _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+            $.src='https://v2.zopim.com/?4h3lbyJihoT1mCOqDA0VoQOaVQE9qTOP';z.t=+new Date;$.
+            type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+          `}
+        </Script>
+      </head>
       <body>
         <AnnouncementBar />
         <Header />
